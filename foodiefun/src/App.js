ac535ac714b3aca73c9750df724ec40a2e3c90b3
andrew dillon <majik895@gmail.com>
2019-07-29 15:50:42 -0400

    initial commit

--- foodiefun/src/App.js
+++ foodiefun/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from './components/Navbar'
+import Form from './components/Form'
+import PageContent from './components/PageContent'
+import { ThemeProvider, ThemeContext } from './components/contexts/ThemeContext'
+import { LanguageProvider } from './components/contexts/LanguageContext'
+
+const App = () => {
+	return (
+		<ThemeProvider>
+			<LanguageProvider>
+				<PageContent>
+					<Navbar />
+					<Form />
+				</PageContent>
+			</LanguageProvider>
+		</ThemeProvider>
+	)
 }
 
-export default App;
+export default App
