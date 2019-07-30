e144e30e6adcbf6ba3b2418d0b499755aa048fed
andrew dillon <majik895@gmail.com>
2019-07-30 13:33:50 -0400

    new form

--- foodiefun/src/App.js
+++ foodiefun/src/App.js
@@ -1,21 +1,12 @@
 import React from 'react'
 
-import Navbar from './components/Navbar'
 import Form from './components/Form'
-import PageContent from './components/PageContent'
-import { ThemeProvider, ThemeContext } from './components/contexts/ThemeContext'
-import { LanguageProvider } from './components/contexts/LanguageContext'
 
 const App = () => {
 	return (
-		<ThemeProvider>
-			<LanguageProvider>
-				<PageContent>
-					<Navbar />
-					<Form />
-				</PageContent>
-			</LanguageProvider>
-		</ThemeProvider>
+		<div className="App">
+			<Form />
+		</div>
 	)
 }
 
