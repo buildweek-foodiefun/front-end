d809494fca41be1570f45c585781f6ca78744a7f
masonkarsevar <masonkarsevar@gmail.com>
2019-07-30 08:48:01 -0700

    state still works from ReviewForm.js

--- foodiefun/src/App.js
+++ foodiefun/src/App.js
@@ -24,9 +24,9 @@ const App = () => {
 				<PageContent>
 					<Navbar />
 					{/* <Form /> */}
-          {/* <ReviewForm addReview={addReview} />
-          {console.log(reviews)} */}
-          <UserInfo data = {mockarray} />
+          <ReviewForm addReview={addReview} />
+          {console.log(reviews)}
+          {/* <UserInfo data = {mockarray} /> */}
 				</PageContent>
 			</LanguageProvider>
 		</ThemeProvider>
